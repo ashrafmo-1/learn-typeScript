e925d231ebdea7fc769faeca9914673e95de08a6
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-01-23 20:33:28 +0200

    solved proplem

--- assignments TypeScript/assignmentTypeScript/five/src/main.ts
+++ assignments TypeScript/assignmentTypeScript/five/src/main.ts
@@ -1,5 +1,5 @@
 function reportErrors(username, age: number) {
-    // let rank = "Professor"; // not using because make an error performance
+    let rank = "Professor"; // not using because make an error performance
     return `Username: ${username}`;
     console.log("We Will Not Reach Here");
 }

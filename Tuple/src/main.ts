e6d04b69ba52fe5d757868486b0f1a8f52bc908d
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-01-24 20:12:49 +0200

    void and never

--- Tuple/src/main.ts
+++ Tuple/src/main.ts
@@ -4,6 +4,7 @@
 you cant add new values by yourself
 can you and values by 'push' and 'unshift'
 readOnly cand make any shanging in arrays
+We Knows Which Types It Contains At Specific Position
 */
 
 let arr: readonly [number, string, boolean] = [21, 'ts', true];

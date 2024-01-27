2434dc82d971ce33e26e1e61337366f8d160997f
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-01-27 23:18:08 +0200

    start

---
+++ get and set accessors/src/main.ts
@@ -0,0 +1,13 @@
+/* Get And Set Accessors */
+
+
+class User {
+    public name: string;
+    public adderss: string;
+    public age: number;
+    constructor(name: string, adderss: string, age: number) {
+        this.name = name;
+        this.adderss = adderss;
+        this.age = age;
+    }
+}

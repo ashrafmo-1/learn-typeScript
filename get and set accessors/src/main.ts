9b083bf238db753ea04af193193456f109d2bd85
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-01-28 01:55:54 +0200

    get and set accessors

--- get and set accessors/src/main.ts
+++ get and set accessors/src/main.ts
@@ -1,13 +1,18 @@
-/* Get And Set Accessors */
+// /* Get And Set Accessors */
 
 
 class User {
-    public name: string;
-    public adderss: string;
-    public age: number;
-    constructor(name: string, adderss: string, age: number) {
-        this.name = name;
-        this.adderss = adderss;
-        this.age = age;
+    constructor(public name: string, public adderss: string, private _age: number) {
+    }
+    get age(): number {
+        return this._age
+    }
+    set age(val: number) {
+        this._age = val
     }
 }
+let person = new User('ashraf', 'alx', 21);
+
+console.log(person.age);
+person.age = 22
+console.log(person.age);
